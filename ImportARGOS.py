a077ad0877afa74cfeca4f7867fcbeb504cd0f4c
kc386 <69813047+kc386@users.noreply.github.com>
2020-10-04 14:42:27 -0400

    Import modules

--- ImportARGOS.py
+++ ImportARGOS.py
@@ -9,3 +9,6 @@
 ## Created: Fall 2020
 ## Author: Kimberly.corcoran@duke.edu (for ENV859)
 ##---------------------------------------------------------------------
+
+# Import modules
+import sys, os, arcpy
